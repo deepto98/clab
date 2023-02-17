9f88e5a99b8c30dad3da90b35490744bbbf28a47
deepto <ddepp98@outlook.com>
2023-02-17 23:26:45 +0530

    Added basic profile page, added getHumanByUsername route

---
+++ src/server/api/routers/human.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
+
+export const HumanRouter = createTRPCRouter({
+  getHumanByUsername: publicProcedure
+    .input(z.object({ username: z.string() }))
+    .query(({ input }) => {
+      return {
+        id: "1234",
+        avatar:"/avatar.png",
+        header:"/header.png",
+        name:"deepto",
+        username:"ddepp98",
+        address:"ddutta98@gmail.com",
+        summary:"",
+        followersCount:1,
+        followingCount:10,
+      };
+    }),
+
+
+});
